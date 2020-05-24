45ffc37235fb0cfb005d5cf5cf21a547ec74dd9c
Mark Vartanyan <kolypto@gmail.com>
2020-05-24 23:20:52 +0400

    Clean-up: removed .css and .spec.ts

--- app-a/src/app/demo-template-reference.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DemoTemplateReference } from './demo-template-reference';
-
-describe('DemoTemplateReference', () => {
-  it('should create an instance', () => {
-    expect(new DemoTemplateReference()).toBeTruthy();
-  });
-});
