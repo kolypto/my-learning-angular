d0bf6cc2270819f4e0994866a20e4614f87066f4
Mark Vartanyan <kolypto@gmail.com>
2020-05-24 23:35:53 +0400

    fix

--- app-a/src/app/demo-template-reference/demo-template-reference.component.ts
+++ app-a/src/app/demo-template-reference/demo-template-reference.component.ts
@@ -4,13 +4,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-demo-template-reference',
   template: `
     <label>Type something:
-      <input #customerInput>{{customerInput.value}}
+      <input #customerInput> {{customerInput.value}}
     </label>
   `,
 })
 export class DemoTemplateReferenceComponent implements OnInit {
-  customerInput: string;
-
   constructor() { }
 
   ngOnInit(): void {
