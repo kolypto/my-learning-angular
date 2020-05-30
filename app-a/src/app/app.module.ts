2bf019039ff6b766855a1d6038fb894e631aab62
Mark Vartanyan <kolypto@gmail.com>
2020-05-31 00:45:01 +0400

    refactor: folders

--- app-a/src/app/app.module.ts
+++ app-a/src/app/app.module.ts
@@ -1,25 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DemoTemplateReferenceComponent } from './heroes/demo-template-reference.component';
-import { DemoHeroListComponent } from './heroes/demo-hero-list.component';
-import { DemoHeroListItemComponent } from "./heroes/demo-hero-list-item.component";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DemoTemplateReferenceComponent} from './heroes/demo-template-reference.component';
+import {DemoHeroListComponent} from './heroes/demo-hero-list.component';
+import {DemoHeroListItemComponent} from "./heroes/demo-hero-list-item.component";
 import {DemoHeroManagerComponent} from "./heroes/demo-hero-manager.component";
-import { HeroListItemStyleDirective } from './hero-list-item-style.directive';
-import { BlinkerDirective } from './heroes/blinker.directive';
+import {HeroListItemStyleDirective} from './heroes/hero-list-item-style.directive';
+import {BlinkerDirective} from './heroes/blinker.directive';
 import {TimersScreenComponent} from "./timer/timers-screen.component";
 import {CountdownTimerComponent} from "./timer/countdown-timer-component";
-import { IndexComponent } from './forms/index.component';
+import {IndexComponent} from './forms/index.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { ReactiveComponent } from './forms/reactive.component';
-import { TemplateComponent } from './forms/template.component';
+import {ReactiveComponent} from './forms/reactive.component';
+import {TemplateComponent} from './forms/template.component';
 import {ForbiddenValidatorDirective} from "./forms/forbidden-name.directive";
 import {IdentityRevealedValidatorDirective} from "./forms/identity-revealed.validator";
-import { DynamicFormQuestionComponent } from './forms/dynamic/dynamic.component';
+import {DynamicFormQuestionComponent} from './forms/dynamic/dynamic.component';
 import {DynamicFormComponent} from "./forms/dynamic/dynamic-form.component";
-import {QuestionService} from "./forms/dynamic/dynamic-question.service";
 
 @NgModule({
   declarations: [
